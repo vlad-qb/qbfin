36a64ea43ee1bdc762d7d521f01cf6ebac9ab7e4
vlad-qb <msgforvlad@yandex.ru>
2015-12-22 11:40:30 +0300

    22.12.2015

--- index.php
+++ index.php
@@ -1,72 +1,111 @@
 <!DOCTYPE html>
-  <html lang="ru">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>QBF ||| investment - компания</title>
-      <link href="/css/reset.css" rel="stylesheet">
-      <link href="/css/grid.css" rel="stylesheet">
-      <link href="/css/typo.css" rel="stylesheet">
-      <link href="/css/icons.css" rel="stylesheet">
-      <link href="/css/bg.css" rel="stylesheet">
-      <link href="/css/gallery.css" rel="stylesheet">
-      <link href="/css/blocks.css" rel="stylesheet">
-      <link href="/css/forms.css" rel="stylesheet">
-	  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,100' rel='stylesheet' type='text/css'>
-      <!-- HTML5 for IE8 -->
-      <!--[if lt IE 9]>
-        <script src="js/html5shiv.min.js"></script>
-        <script src="js/respond.min.js"></script>
-      <![endif]-->
-    </head>
-    <body>
-      <?php include 'html/header.html';?>
-	  
-      <div id="cover" class="img-index-1 cover w100r h480 mt-80" style="background-position: center top;">
-        <div class="column-container" style="height:30px;position:relative;top:450px;">
-          <div class="column-3">
-            <div class="column-offset" style="background:white;height:30px;margin-top:0;">
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QBF ||| investment - компания</title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,100' rel='stylesheet' type='text/css'>
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header.html';?>
+    <div id="cover" class="img-index-1 cover w100r h480 mt-80" style="background-position: center top;">
 
-            </div>
-          </div>
+    </div>
+    <div class="column-container text-center">
 
-          <div class="column-3 no-mobile no-tablet-v">
-            <div class="column-offset" style="background:white;height:30px;margin-top:0;">
-
-            </div>
-          </div>
-		  
-		  <div class="column-3 no-mobile no-tablet-v">
-            <div class="column-offset" style="background:white;height:30px;margin-top:0;">
-
+      <div class="column-3 content-margin">
+        <div class="column-offset bg-white">
+          <h3 class="size-18 color-teal line-h50">СТРАТЕГИИ</h3>
+          <div class="auto line-black line-h70 mt10" style="width:180px;"></div>
+          <h3><a href="/asset-management.php" class="size-14 dark-gray-hover line-h70">УПРАВЛЕНИЕ АКТИВАМИ</a></h3>
+          <div class="img-index-2 cover pt60r" style="background-position: center center;">
+            <div class="bg-label-2">
+              <div class="pt05r pb05r pl05r pr05r">
+                  <div class="l w80r auto pt05r">
+                    <p class="size-12 text-left uppercase line-h20">
+                      <a href="/asset-management.php" class="color-white gray-hover">ЗАЩИТА РУБЛЕВЫХ АКТИВОВ И СТРАТЕГИИ УПРАВЛЕНИЯ ДЕНЬГАМИ</a>
+                    </p>
+                  </div>
+                  <div class="l w20r auto">
+                    <a href="/asset-management.php">
+                      <img src="/img/icons/arrow-right-white.svg" class="icon arrow">
+                    </a>
+                  </div>
+                  <div class="clearfix"></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-	  <div class="column-container w90r mt-35">
-          <div class="column-3 w28r ml025r text-center">
-		    <h3 class="size-18 color-teal line-h70">СТРАТЕГИИ</h3>
-		    <div class="auto line-black line-h70"></div>
-			<h3><a href="/asset-management.php" class="size-14 dark-gray-hover line-h70">УПРАВЛЕНИЕ АКТИВАМИ</a></h3>
-			<div class="img-index-2 h300 cover" style="background-position: center center;"></div>
-		  </div>
-		  <div class="column-3 w28r ml025r text-center">
-		    <h3 class="size-18 color-teal line-h70">КОНСУЛЬТАЦИИ</h3>
-		    <div class="auto line-gray line-h70"></div>
-			<h3><a href="/asset-management.php" class="size-14 dark-gray-hover line-h70">ИНВЕСТИЦИИ И СТРАТЕГИИ</a></h3>
-			<div class="img-index-3 h300 cover" style="background-position: center center;"></div>
-		  </div>
-		  <div class="column-3 w28r ml025r text-center">
-		    <h3 class="size-18 color-teal line-h70">НЕДВИЖИМОСТЬ</h3>
-		    <div class="auto line-gray line-h70"></div>
-			<h3><a href="/asset-management.php" class="size-14 dark-gray-hover line-h70">РЕАЛЬНЫЙ СЕКТОР</a></h3>
-			<div class="img-index-4 h300 cover" style="background-position: center center;"></div>
-		  </div>
+
+      <div class="column-3 content-margin">
+        <div class="column-offset bg-white">
+          <h3 class="size-18 color-teal line-h50">КОНСУЛЬТАЦИИ</h3>
+          <div class="auto line-gray line-h70 mt10" style="width:180px;"></div>
+          <h3><a href="/consulting-management.php" class="size-14 dark-gray-hover line-h70">ИНВЕСТИЦИИ И СТРАТЕГИИ</a></h3>
+          <div class="img-index-3 cover pt60r" style="background-position: center center;">
+            <div class="bg-label-2">
+              <div class="pt05r pb05r pl05r pr05r">
+                <div class="l w80r auto pt05r">
+                  <p class="size-12 text-left uppercase line-h20">
+                    <a href="/consulting-management.php" class="color-white gray-hover">ИНВЕСТИРОВАНИЕ И СТРАТЕГИЧЕСКОЕ ПЛАНИРОВАНИЕ ФИНАНСОВ</a>
+                  </p>
+                </div>
+                <div class="l w20r auto">
+                  <a href="/consulting-management.php">
+                    <img src="/img/icons/arrow-right-white.svg" class="icon arrow">
+                  </a>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="column-3 content-margin">
+        <div class="column-offset bg-white">
+          <h3 class="size-18 color-teal line-h50">НЕДВИЖИМОСТЬ</h3>
+          <div class="auto line-teal line-h70 mt10" style="width:180px;"></div>
+          <h3><a href="/real-estate.php" class="size-14 dark-gray-hover line-h70">РЕАЛЬНЫЙ СЕКТОР</a></h3>
+          <div class="img-index-4 cover pt60r" style="background-position: center center;">
+            <div class="bg-label-2">
+              <div class="pt05r pb05r pl05r pr05r">
+                  <div class="l w80r auto pt05r">
+                    <p class="size-12 text-left uppercase line-h20">
+                      <a href="/real-estate.html" class="color-white gray-hover">ГРИБОВСКИЙ ЛЕС - НЕДВИЖИМОСТЬ В ПОДМОСКОВЬИ</a>
+                    </p>
+                  </div>
+                  <div class="l w20r auto">
+                    <a href="/real-estate.html">
+                      <img src="/img/icons/arrow-right-white.svg" class="icon arrow">
+                    </a>
+                  </div>
+                  <div class="clearfix"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="clearfix"></div>
-	  
-      <?php include 'html/footer.html';?>
-      <script src="/js/jquery.min.js"></script>
-      <script src="/js/main.js"></script>
-    </body>
-  </html>
+    </div>
+    <?php include 'html/footer.html';?>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/main.js"></script>
+  </body>
+</html>
