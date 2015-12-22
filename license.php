4ebf42cc780fd398e66cf81bb4088fab73f19b40
vlad-qb <msgforvlad@yandex.ru>
2015-12-22 18:43:09 +0300

    22.12.2015

---
+++ license.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> QBF ||| investment - официальный сайт </title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,100' rel='stylesheet' type='text/css'>
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header.html';?>
+    <!-- HEADBLOCK -->
+    <div id="cover" 
+          class="img-mission-1 cover w100r h480 mt-80" 
+          style="background-position: center bottom;">
+      
+    </div>
+    <div class="column-container">
+      <div class="column-2 content-margin text-center size-8 line-h24 uppercase">
+        <div class="column-offset bg-white pt05r">
+          <ul>
+            <li class="w15r ml025r mr025r l auto">
+              <a href="/events.php">
+                <img src="img/icons/icon-menu-processes.svg" class="icon icon-menu w100r"><br>
+                <span> события </span>
+              </a>
+            </li>
+            <li class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-call.svg" class="icon icon-menu w100r"><br>
+                <span> звонок </span>
+              </a>
+            </li>
+            <li class="w15r ml025r mr025r l auto">
+              <a href="/service.php">
+                <img src="img/icons/icon-menu-services.svg" class="icon icon-menu w100r"><br>
+                <span> услуги </span>
+              </a>
+            </li>
+            <li class="w15r ml025r mr025r l auto">
+              <a href="/branch-office.php">
+                <img src="img/icons/icon-menu-affiliates.svg" class="icon icon-menu w100r"><br>
+                <span> филиалы </span>
+              </a>
+            </li>
+            <li class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-mail.svg" class="icon icon-menu w100r"><br>
+                <span> почта </span>
+              </a>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+          <div class="size-14 color-teal line-h70 mt10">
+            <span class="weight-300"> +7 (495) 123 45 67 </span> 
+            <span class="color-grey1 ml005r mr005r"> ~ </span> 
+            <span class="weight-300"> INVEST@QBFIN.RU </span>
+          </div>
+          <table class="w85r auto mt15">
+            <tr>
+              <td class="line-black"></td>
+              <td class="line-gray"></td>
+              <td class="line-teal"></td>
+            </tr>
+          </table>
+          <div class="w85r auto size-12 color-grey1 uppercase mt30">
+            <ul>
+              <li class="inline">
+                <a class="size-12" href="/company.php"> КОМПАНИЯ </a>
+                <span class="color-black">|</span><span class="color-gray">|</span><span class="color-teal">|</span>
+              </li>
+              <li class="inline">
+                <a href="/about.php"> О НАС </a>
+                <span class="color-teal"> • </span>
+              </li>
+              <li class="inline">
+                <a href="/mission.php"> МИССИЯ И ЦЕННОСТИ </a>
+                <span class="color-teal"> • </span>
+              </li>
+              <li class="inline">
+                <a href="/history.php"> ИСТОРИЯ </a>
+                <span class="color-teal"> • </span>
+              </li>
+              <li class="inline">
+                <a href="/team.php"> РУКОВОДСТВО </a>
+                <span class="color-teal"> • </span>
+              </li>
+              <li class="inline">
+                <a href="/clients-and-partners.php"> КЛИЕНТЫ И ПАРТНЕРЫ </a>
+                <span class="color-teal">•</span>
+              </li>
+              <li class="inline">
+                <a href="/awards-and-ratings.php"> НАГРАДЫ И РЕЙТИНГИ </a>
+                <span class="color-teal">•</span>
+              </li>
+              <li class="inline">
+                <a href="/branch-office.php"> ФИЛИАЛЫ </a>
+                <span class="color-teal"> • </span>
+              </li>
+              <li class="inline">
+                <a href="/documents.php"> ДОКУМЕНТЫ </a>
+              </li>
+            </ul>
+          </div> 
+      </div>
+    </div>
+    <div class="column-2 content-margin">
+      <div class="column-offset bg-white text-center pt05r">
+        <h2 class="size-18 line-h50 color-teal uppercase"> ЛИЦЕНЗИИ </h2>
+        <p class="size-11 line-h20 text-center mt30 pl05r pr05r">
+          <span class="bold"> ОРИЕНТИРОВАННЫЙ НА КЛИЕНТА ПОДХОД К ВЕДЕНИЮ БРОКЕРСКОГО БИЗНЕСА </span>
+        </p>
+        <p class="weight-300 text-left size-14 line-h22 mt10 pl05r pr05r pb05r">
+          <span> надежность </span>
+          <span class="size-10 color-teal pl05 pr05"> •︎ </span>
+          <span> полная конфиденциальность </span>
+          <span class="size-10 color-teal pl05 pr05"> •︎ </span>
+          <span> высокая квалификация наших сотрудников </span>
+          <span class="size-10 color-teal pl05 pr05"> •︎ </span>
+          <span> актуальность предоставляемых данных </span>
+          <span class="size-10 color-teal pl05 pr05"> •︎ </span>
+          <span> умение предвидеть новые деловые перспективы </span>
+          <span class="size-10 color-teal pl05 pr05"> •︎ </span>
+          <span> широкий спектр возможностей для увеличения прибыли </span>
+        </p>
+        <div class="w100r mt05r text-center">
+          <ul class="social-offset auto">
+            <li class="w25r l">
+              <a href="http://twitter.com/qb_finance" target="_blank"><img alt="twitter" src="img/icons/icon-twitter.svg" class="icon icon-social"></a>
+            </li>
+            <li class="w25r l">
+              <a href="http://www.facebook.com/qbfin.ru" target="_blank"><img alt="facebook" src="img/icons/icon-facebook.svg" class="icon icon-social"></a>
+            </li>
+            <li class="w25r l">
+              <a href="http://www.linkedin.com/company/qb-finance" target="_blank"><img alt="linkedin" src="img/icons/icon-linkedin.svg" class="icon icon-social"></a>
+            </li>
+            <li class="w25r l">
+              <a href="http://www.google.com/+QbfinRu" target="_blank"><img alt="google" src="img/icons/icon-google.svg" class="icon icon-social"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /HEADBLOCK -->
+  <div class="clearfix"></div>
+  <!-- CONTENT -->
+  <div id="licenses" class="column-container text-center">
+    <div class="column-3 pt10r">
+      <div class="column-offset">
+        <img class="w280 h400 auto block" src="/img/resources/license-2.jpg">
+        <h2 class="size-12 line-h40 uppercase"> лицензия: № 077-12805-100000 </h2>
+        <h2 class="size-12 uppercase bold"> брокерская деятельность </h2>
+      </div>
+    </div>
+    <div class="column-3 pt10r">
+      <div class="column-offset">
+        <img class="w280 h400 auto block" src="/img/resources/license-2.jpg">
+        <h2 class="size-12 line-h40 uppercase"> лицензия: № 077-12816-010000 </h2>
+        <h2 class="size-12 uppercase bold"> дилерская деятельность </h2>
+      </div>
+    </div>
+    <div class="column-3 pt10r">
+      <div class="column-offset">
+        <img class="w280 h400 auto block" src="/img/resources/license-2.jpg">
+        <h2 class="size-12 line-h40 uppercase"> лицензия: № 077-12828-001000 </h2>
+        <h2 class="size-12 uppercase bold"> управление ценными бумагами </h2>
+      </div>
+    </div>
+  </div>
+  <!-- /CONTENT -->
+  <!-- FOOTER BLOCK -->
+  <div class="clearfix"></div>
+  <div class="w100r">
+    <div class="text-center mt80">
+      <img src="img/icons/icon-lighthouse.svg" class="icon icon-main"><br>
+      <a href="/company.php" class="size-12 line-h40 uppercase">Компания</a>
+      <span class="color-teal"> • </span>
+      <a href="/documents.php" class="size-12 line-h40 uppercase">Документы</a>
+    </div>    
+  </div>
+  <!-- /FOOTER BLOCK -->
+  <div class="clearfix"></div>
+  <?php include 'html/footer.html';?>
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/main.js"></script>
+</body>
+</html>
