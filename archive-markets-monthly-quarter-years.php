7c5f97f213d5b9bf9ff92e6e98d2dcb7f0ee7c14
meradlin <zawersia@gmail.com>
2015-12-23 17:03:37 +0300

    23/12
    commit

---
+++ archive-markets-monthly-quarter-years.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+  <html lang="ru">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>QBF ||| investment - архив : рынки за месяц, квартал и год</title>
+      <link href="/css/reset.css" rel="stylesheet">
+      <link href="/css/grid.css" rel="stylesheet">
+      <link href="/css/typo.css" rel="stylesheet">
+      <link href="/css/icons.css" rel="stylesheet">
+      <link href="/css/bg.css" rel="stylesheet">
+      <link href="/css/gallery.css" rel="stylesheet">
+      <link href="/css/blocks.css" rel="stylesheet">
+      <link href="/css/forms.css" rel="stylesheet">
+      <!-- HTML5 for IE8 -->
+      <!--[if lt IE 9]>
+        <script src="js/html5shiv.min.js"></script>
+        <script src="js/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
+      <?php include 'html/header.html';?>
+      <div id="cover" class="img-archive-markets cover w100r h480 mt-80" style="background-position: center top;">
+        <div class="column-container h30" style="position:relative;top:450px;">
+          <div class="column-2">
+            <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+
+            </div>
+          </div>
+
+          <div class="column-2 no-mobile no-tablet-v">
+            <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column-container">
+          <div class="column-2">
+            <div class="column-offset" style="margin-top:0;">
+              <div class="mt35 auto text-center size-8 line-h24 uppercase">
+                <p class="w15r ml025r mr025r l auto">
+                  <a href="/events.php">
+                    <img src="img/icons/icon-menu-processes.svg" class="icon icon-menu w100r"><br>
+                    события
+                  </a>
+                </p>
+                <p class="w15r ml025r mr025r l auto">
+                  <a href="#">
+                    <img src="img/icons/icon-menu-call.svg" class="icon icon-menu w100r"><br>
+                    звонок
+                  </a>
+                </p>
+                <p class="w15r ml025r mr025r l auto">
+                  <a href="/service.php">
+                    <img src="img/icons/icon-menu-services.svg" class="icon icon-menu w100r"><br>
+                    услуги
+                  </a>
+                </p>
+                <p class="w15r ml025r mr025r l auto">
+                  <a href="/branch-office.php">
+                    <img src="img/icons/icon-menu-affiliates.svg" class="icon icon-menu w100r"><br>
+                    филиалы
+                  </a>
+                </p>
+                <p class="w15r ml025r mr025r l auto">
+                  <a href="#">
+                    <img src="img/icons/icon-menu-mail.svg" class="icon icon-menu w100r"><br>
+                    почта
+                  </a>
+                </p>
+                <div class="clearfix"></div>
+                <div class="size-14 color-teal line-h70 mt10 weight-300">
+                    +7 (495) 123 45 67 <span class="color-grey1 ml005r mr005r"> ~ </span> INVEST@QBFIN.RU
+                </div>
+                <table class="w85r auto mt15">
+                   <tbody><tr>
+                    <td class="line-black"></td>
+                    <td class="line-gray"></td>
+                    <td class="line-teal"></td>
+                   </tr>
+                  </tbody>
+                </table>
+                <div class="w85r auto size-12 color-grey1 uppercase mt30">
+                  <a class="size-12" href="/analytics.php">АНАЛИТИКА</a>
+                  <span class="color-black">|</span><span class="color-gray">|</span><span class="color-teal">|</span>
+                  <a href="/markets.php">РЫНКИ</a>
+                  <span class="color-teal">•</span>
+                  <a href="/shares.php">АКЦИИ</a>
+                  <span class="color-teal">•</span>
+                  <a href="/reviews.php">ОБЗОРЫ</a>
+                  <span class="color-teal">•</span>
+                  <a href="/expert.php">ЭКСПЕРТ</a>
+                  <span class="color-teal">•</span>
+                  <a href="/archive-analytics.php">АРХИВ</a>
+                </div> 
+              </div>
+            </div>
+          </div>
+          <div class="column-2">
+            <div class="column-offset h30 bg-white" style="margin-top:0;">
+              <div class="w85r auto">
+                <h2 class="size-18 color-teal uppercase text-center line-h110">АРХИВ : РЫНКИ ЗА МЕСЯЦ И КВАРТАЛ</h2>
+                <p class="size-11 line-h20 text-justify">
+                  <span class="bold">ВЫСОКОКВАЛИФИЦИРОВАННАЯ КОМАНДА МЕНЕДЖЕРОВ И ПОРТФЕЛЬНЫХ УПРАВЛЯЮЩИХ, ИМЕЮЩАЯ ЗНАЧИТЕЛЬНЫЙ ОПЫТ РАБОТЫ НА РЫНКЕ ЦЕННЫХ БУМАГ.</span>
+                </p>
+                <div class="w70r mt50 text-center auto">
+                  <p class="w20r ml025r mr025r l auto">
+                    <a href="http://twitter.com/qb_finance" target="_blank"><img alt="twitter" src="img/icons/icon-twitter.svg" class="icon icon-social"></a>
+                  </p>
+                  <p class="w20r ml025r mr025r l auto">
+                    <a href="http://www.facebook.com/qbfin.ru" target="_blank"><img alt="facebook" src="img/icons/icon-facebook.svg" class="icon icon-social"></a>
+                  </p>
+                  <p class="w20r ml025r mr025r l auto">
+                    <a href="http://www.linkedin.com/company/qb-finance" target="_blank"><img alt="linkedin" src="img/icons/icon-linkedin.svg" class="icon icon-social"></a>
+                  </p>
+                  <p class="w20r ml025r mr025r l auto">
+                    <a href="http://www.google.com/+QbfinRu" target="_blank"><img alt="google" src="img/icons/icon-google.svg" class="icon icon-social"></a>
+                  </p>
+                </div>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+      </div>
+	<div class="clearfix"></div>
+
+<!-- /markets month -->	
+<div class="column-container w87r auto">
+    <div class="column mt70">
+        <div class="column-offset">
+            <h3 class="text-center size-12 color-white"><div class="w288 h40 line-h40 auto bg-teal">РЫНКИ ЗА МЕСЯЦ</div></h3>
+            <div class="title-line-teal"></div>
+        </div>
+    </div>
+</div>
+<div id="text-feed" class="column-container size-14 line-h26">
+    <div class="column-2">
+        <div class="column-offset">
+            <p class="mt30"></p>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2015
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">январь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">февраль</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">март</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">апрель</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">май</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">июнь</a>
+                </div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">июль</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">август</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">сентябрь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">октябрь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">ноябрь</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300 color-whitesmoke">декабрь</a>
+                </div>
+        </div>
+    </div>
+    <div class="column-2">
+        <div class="column-offset">
+            <p class="mt30"></p>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2014
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">январь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">февраль</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">март</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">апрель</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">май</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">июнь</a>
+                </div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">июль</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">август</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">сентябрь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">октябрь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">ноябрь</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">декабрь</a>
+                </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+<!-- /markets month -->	  
+
+<!-- /markets quartals -->	
+<div class="column-container w87r auto">
+    <div class="column mt60">
+        <div class="column-offset">
+            <h3 class="text-center size-12 color-white"><div class="w288 h40 line-h40 auto bg-teal">РЫНКИ ЗА КВАРТАЛ</div></h3>
+            <div class="title-line-teal"></div>
+        </div>
+    </div>
+</div>
+<div id="text-feed" class="column-container size-14 line-h26">
+    <div class="column-2">
+        <div class="column-offset">
+            <p class="mt30"></p>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2015
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">1 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">2 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">3 квартал</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300 color-whitesmoke"> 4 квартал</a>
+                </div>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2013
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">1 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">2 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">3 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300"> 4 квартал</a>
+                </div>
+        </div>
+    </div>
+    <div class="column-2">
+        <div class="column-offset">
+            <p class="mt30"></p>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2014
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">1 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">2 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">3 квартал</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300 color-whitesmoke"> 4 квартал</a>
+                </div>
+				<div class="line-h50 size-18 weight-300 color-teal text-center">
+                    2012
+				</div>
+				<div class="line-h50 weight-300 ml30 mr30 text-center">
+                    <a href="/markets-review-08-2015.php" class="weight-300">1 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">2 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300">3 квартал</a> <span class="color-teal size-11">•︎</span> <a href="/markets-review-08-2015.php" class="weight-300"> 4 квартал</a>
+                </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+<!-- /markets quartals -->	  
+
+<div class="clearfix"></div>
+<div class="w90r auto">
+      <div class="text-center mt60">
+        <img src="img/icons/icon-lighthouse.svg" class="icon icon-main"><br>
+        <a href="/analytics.php"><span class="size-14">< </span></a><a href="/analytics.php" class="size-12 line-h40 uppercase">Аналитика</a> <span class="color-teal size-10">•︎</span> <a href="/markets.php" class="size-12 line-h40 uppercase">Рынки</a><a href="/markets.php"><span class="size-14"> ></span>
+      </div>	  
+</div>
+      <div class="clearfix"></div>
+      <?php include 'html/footer.html';?>
+      <script src="/js/jquery.min.js"></script>
+      <script src="/js/main.js"></script>
+    </body>
+  </html>
