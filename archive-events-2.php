5fab6c82af414cd80a71110354367735419c521a
meradlin <zawersia@gmail.com>
2015-12-23 15:46:12 +0300

    work
    1

---
+++ archive-events-2.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QBF ||| investment - архив : события - 2</title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header.html';?>
+    <div id="cover" class="img-archive-markets cover w100r h480 mt-80" style="background-position: center top;">
+      <div class="column-container h30" style="position:relative;top:450px;">
+        <div class="column-2">
+          <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+          </div>
+        </div>
+        <div class="column-2 no-mobile no-tablet-v">
+          <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-container">
+      <div class="column-2">
+        <div class="column-offset" style="margin-top:0;">
+          <div class="mt35 auto text-center size-8 line-h24 uppercase">
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/events.php">
+                <img src="img/icons/icon-menu-processes.svg" class="icon icon-menu w100r"><br>
+                события
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-call.svg" class="icon icon-menu w100r"><br>
+                звонок
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/service.php">
+                <img src="img/icons/icon-menu-services.svg" class="icon icon-menu w100r"><br>
+                услуги
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/branch-office.php">
+                <img src="img/icons/icon-menu-affiliates.svg" class="icon icon-menu w100r"><br>
+                филиалы
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-mail.svg" class="icon icon-menu w100r"><br>
+                почта
+              </a>
+            </p>
+            <div class="clearfix"></div>
+            <div class="size-14 color-teal line-h70 mt10 weight-300">
+              +7 (495) 123 45 67 <span class="color-grey1 ml005r mr005r"> ~ </span> INVEST@QBFIN.RU
+            </div>
+            <table class="w85r auto mt15">
+              <tbody><tr>
+                <td class="line-black"></td>
+                <td class="line-gray"></td>
+                <td class="line-teal"></td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="w85r auto size-12 color-grey1 uppercase mt30">
+            <a class="size-12" href="/news.php">НОВОСТИ</a>
+            <span class="color-black">|</span><span class="color-gray">|</span><span class="color-teal">|</span>
+            <a href="/events.php">СОБЫТИЯ</a>
+            <span class="color-teal">•</span>
+            <a href="/laws.php">ЗАКОНОДАТЕЛЬСТВО</a>
+            <span class="color-teal">•</span>
+            <a href="/press-about-us.php">ПРЕССА О НАС</a>
+            <span class="color-teal">•</span>
+            <a href="/press-company.php">КОМПАНИЯ В СМИ</a>
+            <span class="color-teal">•</span>
+            <a href="/archive-news.php">АРХИВ</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-2">
+      <div class="column-offset" style="background:white;height:30px;margin-top:0;">
+        <h2 class="size-18 color-teal uppercase text-center line-h110">АРХИВ : СОБЫТИЯ 2012</h2>
+        <p class="size-11 line-h20 ml40 mr40 text-justify">
+          <span class="bold">ВЫСОКОКВАЛИФИЦИРОВАННАЯ КОМАНДА МЕНЕДЖЕРОВ И ПОРТФЕЛЬНЫХ УПРАВЛЯЮЩИХ, ИМЕЮЩАЯ ЗНАЧИТЕЛЬНЫЙ ОПЫТ РАБОТЫ НА РЫНКЕ ЦЕННЫХ БУМАГ.</span>
+        </p>
+        <div class="w70r mt50 text-center auto">
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://twitter.com/qb_finance" target="_blank"><img alt="twitter" src="img/icons/icon-twitter.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.facebook.com/qbfin.ru" target="_blank"><img alt="facebook" src="img/icons/icon-facebook.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.linkedin.com/company/qb-finance" target="_blank"><img alt="linkedin" src="img/icons/icon-linkedin.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.google.com/+QbfinRu" target="_blank"><img alt="google" src="img/icons/icon-google.svg" class="icon icon-social"></a>
+          </p>
+        </div>
+		
+		<div class="gallery-offset w85r auto mt50">
+		<h3 class="text-center size-18"><div class="w288 h40 line-h40 auto bg-white weight-300"><a href="/archive-events-5.php" class="gray-hover">2015</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-4.php" class="gray-hover">2014</a> <span class="color-whitesmoke size-11">•︎</span> <a href="archive-events-3.php" class="gray-hover">2013</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-2.php" class="color-teal gray-hover">2012</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-1.php" class="gray-hover">2011</a></div></h3>
+		</div>
+		
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix"></div>
+
+<!-- CONTENT -->
+<div id="gallery" class="column-container mt60">
+  <div class="column">
+    <div class="gallery-offset w95r auto line-h24 size-14">
+        <div class="text-center"><img class="icon icon-portfolio" src="/img/bg/archive-shares-3.gif"></div>
+        <div class="title-line-gray"></div>
+	</div>
+  </div>
+  <div class="clearfix"></div>
+  <div class="column-2 mt50">
+    <div class="gallery-offset w85r line-h24 size-14 ml05r mr20r">
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">03.09.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2782798" class="gray-hover">Программа летней стажировки "Quality Benefit Summer 2012"</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">22.07.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">QB Finance выиграла конкурс "Регионы - устойчивое развитие"</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">20.07.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759013" class="gray-hover">За второй квартал 2012 года доходность на инвестиции составила 7,1%</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">16.07.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Cборная QB в матче против команды Егора Титова</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">12.06.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Фонд венчурных инвестиций QB Venture</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">27.05.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">QB Finance расширяет присутствие на российском и украинском рынке</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">22.05.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759013" class="gray-hover">Quality Benefit Summer 2012</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">16.05.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">VI Национальный Экономический Саммит - «Россия: молодежь и предпринимательство»</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">26.04.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">QB Finance подарила себе на День Рождения корпоративную премию</a></h4>
+    </div>
+  </div>
+  <div class="column-2 mt50">
+    <div class="gallery-offset w85r line-h24 size-14 ml05r mr20r">
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">30.03.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759922" class="gray-hover">QB Finance - спонсор ХХIV Международной выставки "Финансовый супермаркет - 2012"</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">21.03.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Экипаж компании QB Finance принял участие в трофи-рейде проекта «Волок.Выходные»</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">19.03.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Состоялся Кубок QB Finance по лыжным гонкам среди детей</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">10.03.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Мария Комиссарова завоевала серебро на кубке мира</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">24.02.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Сергей Бочарников в составе команды Москвы занял 1 место в смешанной эстафете</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">17.02.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759013" class="gray-hover">В 2011 году доходность от инвестирования составила 37% годовых</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">04.02.2012</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">QB Finance приносит успех</a></h4>
+    </div>
+  </div>
+  <div class="clearfix"></div>
+ 
+</div>
+
+<div class="gallery-offset w85r auto mt50">
+<h3 class="text-center size-18"><div class="w288 h40 line-h40 auto bg-white weight-300"><a href="/archive-events-5.php" class="gray-hover">2015</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-4.php" class="gray-hover">2014</a> <span class="color-whitesmoke size-11">•︎</span> <a href="archive-events-3.php" class="gray-hover">2013</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-2.php" class="color-teal gray-hover">2012</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-1.php" class="gray-hover">2011</a></div></h3>
+<div class="title-line-gray2"></div>
+</div>
+
+<div class="clearfix"></div>
+<div class="w100r">
+      <div class="text-center mt80">
+        <img src="img/icons/icon-lighthouse.svg" class="icon icon-main"><br>
+        <a href="/news.php"><span class="size-14">< </span></a><a href="/news.php" class="size-12 line-h40 uppercase">Новости</a> <span class="color-teal size-10">•︎</span> <a href="/events.php" class="size-12 line-h40 uppercase">СОБЫТИЯ</a><a href="/events.php"><span class="size-14"> ></span>
+      </div>	  
+</div>	  
+<div class="clearfix"></div>
+
+<?php include 'html/footer.html';?>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/main.js"></script>
+</body>
+</html>
---
+++ archive-events-3.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QBF ||| investment - архив : события - 3</title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header.html';?>
+    <div id="cover" class="img-archive-markets cover w100r h480 mt-80" style="background-position: center top;">
+      <div class="column-container h30" style="position:relative;top:450px;">
+        <div class="column-2">
+          <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+          </div>
+        </div>
+        <div class="column-2 no-mobile no-tablet-v">
+          <div class="column-offset h30 mt0 bg-white" style="margin-top:0;">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-container">
+      <div class="column-2">
+        <div class="column-offset" style="margin-top:0;">
+          <div class="mt35 auto text-center size-8 line-h24 uppercase">
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/events.php">
+                <img src="img/icons/icon-menu-processes.svg" class="icon icon-menu w100r"><br>
+                события
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-call.svg" class="icon icon-menu w100r"><br>
+                звонок
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/service.php">
+                <img src="img/icons/icon-menu-services.svg" class="icon icon-menu w100r"><br>
+                услуги
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="/branch-office.php">
+                <img src="img/icons/icon-menu-affiliates.svg" class="icon icon-menu w100r"><br>
+                филиалы
+              </a>
+            </p>
+            <p class="w15r ml025r mr025r l auto">
+              <a href="#">
+                <img src="img/icons/icon-menu-mail.svg" class="icon icon-menu w100r"><br>
+                почта
+              </a>
+            </p>
+            <div class="clearfix"></div>
+            <div class="size-14 color-teal line-h70 mt10 weight-300">
+              +7 (495) 123 45 67 <span class="color-grey1 ml005r mr005r"> ~ </span> INVEST@QBFIN.RU
+            </div>
+            <table class="w85r auto mt15">
+              <tbody><tr>
+                <td class="line-black"></td>
+                <td class="line-gray"></td>
+                <td class="line-teal"></td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="w85r auto size-12 color-grey1 uppercase mt30">
+            <a class="size-12" href="/news.php">НОВОСТИ</a>
+            <span class="color-black">|</span><span class="color-gray">|</span><span class="color-teal">|</span>
+            <a href="/events.php">СОБЫТИЯ</a>
+            <span class="color-teal">•</span>
+            <a href="/laws.php">ЗАКОНОДАТЕЛЬСТВО</a>
+            <span class="color-teal">•</span>
+            <a href="/press-about-us.php">ПРЕССА О НАС</a>
+            <span class="color-teal">•</span>
+            <a href="/press-company.php">КОМПАНИЯ В СМИ</a>
+            <span class="color-teal">•</span>
+            <a href="/archive-news.php">АРХИВ</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-2">
+      <div class="column-offset" style="background:white;height:30px;margin-top:0;">
+        <h2 class="size-18 color-teal uppercase text-center line-h110">АРХИВ : СОБЫТИЯ 2013</h2>
+        <p class="size-11 line-h20 ml40 mr40 text-justify">
+          <span class="bold">ВЫСОКОКВАЛИФИЦИРОВАННАЯ КОМАНДА МЕНЕДЖЕРОВ И ПОРТФЕЛЬНЫХ УПРАВЛЯЮЩИХ, ИМЕЮЩАЯ ЗНАЧИТЕЛЬНЫЙ ОПЫТ РАБОТЫ НА РЫНКЕ ЦЕННЫХ БУМАГ.</span>
+        </p>
+        <div class="w70r mt50 text-center auto">
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://twitter.com/qb_finance" target="_blank"><img alt="twitter" src="img/icons/icon-twitter.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.facebook.com/qbfin.ru" target="_blank"><img alt="facebook" src="img/icons/icon-facebook.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.linkedin.com/company/qb-finance" target="_blank"><img alt="linkedin" src="img/icons/icon-linkedin.svg" class="icon icon-social"></a>
+          </p>
+          <p class="w20r ml025r mr025r l auto">
+            <a href="http://www.google.com/+QbfinRu" target="_blank"><img alt="google" src="img/icons/icon-google.svg" class="icon icon-social"></a>
+          </p>
+        </div>
+		
+		<div class="gallery-offset w85r auto mt50">
+		<h3 class="text-center size-18"><div class="w288 h40 line-h40 auto bg-white weight-300"><a href="/archive-events-5.php" class="gray-hover">2015</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-4.php" class="gray-hover">2014</a> <span class="color-whitesmoke size-11">•︎</span> <a href="archive-events-3.php" class="color-teal gray-hover">2013</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-2.php" class="gray-hover">2012</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-1.php" class="gray-hover">2011</a></div></h3>
+		</div>
+		
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix"></div>
+
+<!-- CONTENT -->
+<div id="gallery" class="column-container mt60">
+  <div class="column">
+    <div class="gallery-offset w95r auto line-h24 size-14">
+        <div class="text-center"><img class="icon icon-portfolio" src="/img/bg/archive-shares-2.gif"></div>
+        <div class="title-line-gray"></div>
+	</div>
+  </div>
+  <div class="clearfix"></div>
+  <div class="column-2 mt50">
+    <div class="gallery-offset w85r line-h24 size-14 ml05r mr20r">
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">07.12.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2792106" class="gray-hover">Встреча с президентом ФУ при правительстве РФ</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">06.12.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2790799" class="gray-hover">QB Finance - лауреат премии SPEAR'S Russia Wealth Management Awards 2013</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">07.10.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2790762" class="gray-hover">QB Finance участвует в Spear's Russia WMA 2013</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">24.09.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Эксперты QB Finance на портале «Банки.ру»</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">10.07.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Партнерское соглашение с компанией Apex Capital Partners Corp.</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">10.07.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Открытие сезона в гольф-клубе «Целеево»</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">27.05.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759013" class="gray-hover">Презентация филиала QB Finance в Белгороде - II раунд</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">20.05.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">QB Finance Ukraine порадует жителей элитного коттеджного комплекса</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">16.05.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">QB Finance поддержит открытие сезона 2013 Гольф-клуба «Целеево»!</a></h4>
+    </div>
+  </div>
+  <div class="column-2 mt50">
+    <div class="gallery-offset w85r line-h24 size-14 ml05r mr20r">
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">07.05.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759922" class="gray-hover">Стартует следующий этап презентации отдела алгоритмической торговли компании QB Finance!</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">25.04.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Презентация филиала инвестиционной компании QB Finance в Белгороде</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">20.04.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Чемпионат Краснодарского края по мотокроссу и этап Чемпионата Европы по мотокроссу</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">12.04.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">Конференция «Управление частым капиталом и Private Banking» в отеле «Балчуг Кемпински»</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">11.04.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759058" class="gray-hover">Конференция - FinPlace</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">31.03.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2759013" class="gray-hover">31 марта в "Метелице" состоялась закрытая презентация джазового альбома Сергея Минаева</a></h4>
+		<h4 class="size-14 ml05r line-h30 weight-300"><span class="color-teal weight-300">01.03.2013</span> <span class="size-11 color-lightgray"> •︎ </span><a href="http://www.kommersant.ru/doc/2758982" class="gray-hover">QB Finance - Лучший работодатель России!</a></h4>
+    </div>
+  </div>
+  <div class="clearfix"></div>
+ 
+</div>
+
+<div class="gallery-offset w85r auto mt50">
+<h3 class="text-center size-18"><div class="w288 h40 line-h40 auto bg-white weight-300"><a href="/archive-events-5.php" class="gray-hover">2015</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-4.php" class="gray-hover">2014</a> <span class="color-whitesmoke size-11">•︎</span> <a href="archive-events-3.php" class="color-teal gray-hover">2013</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-2.php" class="gray-hover">2012</a> <span class="color-whitesmoke size-11">•︎</span> <a href="/archive-events-1.php" class="gray-hover">2011</a></div></h3>
+<div class="title-line-gray2"></div>
+</div>
+
+<div class="clearfix"></div>
+<div class="w100r">
+      <div class="text-center mt80">
+        <img src="img/icons/icon-lighthouse.svg" class="icon icon-main"><br>
+        <a href="/news.php"><span class="size-14">< </span></a><a href="/news.php" class="size-12 line-h40 uppercase">Новости</a> <span class="color-teal size-10">•︎</span> <a href="/events.php" class="size-12 line-h40 uppercase">СОБЫТИЯ</a><a href="/events.php"><span class="size-14"> ></span>
+      </div>	  
+</div>	  
+<div class="clearfix"></div>
+
+<?php include 'html/footer.html';?>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/main.js"></script>
+</body>
+</html>
