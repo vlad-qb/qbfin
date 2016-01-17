051e8886a575a1a579bb34a63264e30309f154f7
vlad-qb <msgforvlad@yandex.ru>
2016-01-17 23:37:26 +0300

    success,error,search-result

---
+++ search-results.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QBF ||| investment - официальный сайт</title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <link href="/css/theme.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,100' rel='stylesheet' type='text/css'>
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header-connect.html';?>
+    <!-- HEADBLOCK -->
+    <div id="cover"
+      class="img-search-1 w100r h480 mt-80"
+      style="background-position: -50px center;">
+    </div>
+    <div id="headblock" class="column-container">
+        <div class="column-2 content-margin text-center size-8 line-h24 uppercase">
+          <div class="column-offset bg-white pt35">
+            <ul>
+              <li class="w15r ml025r mr025r l auto lightgray-block-hover3">
+                <a href="/events.php">
+                  <img src="img/icons/icon-menu-processes.svg" class="icon icon-menu w100r"><br>
+                  <span> события </span>
+                </a>
+              </li>
+              <li class="w15r ml025r mr025r l auto lightgray-block-hover3">
+                <a href="#">
+                  <img src="img/icons/icon-menu-call.svg" class="icon icon-menu w100r"><br>
+                  <span> звонок </span>
+                </a>
+              </li>
+              <li class="w15r ml025r mr025r l auto lightgray-block-hover3">
+                <a href="/service.php">
+                  <img src="img/icons/icon-menu-services.svg" class="icon icon-menu w100r"><br>
+                  <span> услуги </span>
+                </a>
+              </li>
+              <li class="w15r ml025r mr025r l auto lightgray-block-hover3">
+                <a href="/branch-office.php">
+                  <img src="img/icons/icon-menu-affiliates.svg" class="icon icon-menu w100r"><br>
+                  <span> филиалы </span>
+                </a>
+              </li>
+              <li class="w15r ml025r mr025r l auto lightgray-block-hover3">
+                <a href="#">
+                  <img src="img/icons/icon-menu-mail.svg" class="icon icon-menu w100r"><br>
+                  <span> почта </span>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+            <div class="size-14 color-teal line-h50 mt10">
+              <span class="weight-300"> +7 (495) 123 45 67 </span> 
+              <span class="color-grey1 ml005r mr005r"> ~ </span> 
+              <span class="weight-300"> INVEST@QBFIN.RU </span>
+            </div>
+            <table class="w85r auto mt15">
+              <tr>
+                <td class="line-black"></td>
+                <td class="line-gray"></td>
+                <td class="line-teal"></td>
+              </tr>
+            </table>
+            <div class="w85r auto size-12 color-grey1 uppercase mt30">
+              <ul>
+                <li class="inline">
+                  <a class="size-12" href="/company.php"> КОМПАНИЯ </a>
+                </li>
+                <li class="inline">
+                  <a href="/service.php"> УСЛУГИ </a>
+                  <span class="color-teal"> • </span>
+                </li>
+                <li class="inline">
+                  <a href="/analytics/"> АНАЛИТИКА </a>
+                  <span class="color-teal"> • </span>
+                </li>
+                <li class="inline">
+                  <a href="/news/"> НОВОСТИ </a>
+                  <span class="color-teal"> • </span>
+                </li>
+                <li class="inline">
+                  <a href="/career.php"> КАРЬЕРА </a>
+                  <span class="color-teal"> • </span>
+                </li>
+                <li class="inline">
+                  <a href="/connect.php"> КОНТАКТЫ </a>
+                  <span class="color-teal">•</span>
+                </li>
+              </ul>
+            </div> 
+        </div>
+      </div>
+      <div class="column-2 content-margin">
+        <div class="column-offset bg-white text-center pt50">
+          <h2 class="title"> РЕЗУЛЬТАТЫ ПОИСКА </h2>
+          <p class="description">
+            <span class="company pr05">QBF ||| investment</span><span class="sep"> - </span> ИНВЕСТИЦИОННАЯ КОМПАНИЯ, ПРЕДОСТАВЛЯЮЩАЯ УСЛУГИ ПО УПРАВЛЕНИЮ АКТИВАМИ НА ФИНАНСОВЫХ РЫНКАХ РОССИИ, ЕВРОПЫ И СЕВЕРНОЙ АМЕРИКИ.
+          </p>
+          <div class="w100r mt05r text-center">
+            <ul class="social-offset auto hi-icon-wrap hi-icon-effect-8">
+              <li class="w25r l">
+                <a class="hi-icon icon-tw" href="http://twitter.com/qb_finance"></a>
+              </li>
+              <li class="w25r l">
+                <a class="hi-icon icon-fb" href="http://www.facebook.com/qbfin.ru"></a>
+              </li>
+              <li class="w25r l">
+                <a class="hi-icon icon-ln" href="http://www.linkedin.com/company/qb-finance"></a>
+              </li>
+              <li class="w25r l">
+                <a class="hi-icon icon-g" href="http://www.google.com/+QbfinRu"></a>
+              </li>
+              <div class="clearfix"></div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <!-- /HEADBLOCK -->
+    <div id="content">
+      <div class="column-container pt05r">
+        <div class="column">
+          <div id="search-result" class="column-offset">
+            <div class="size-14 line-h20 ml05r mr05r pb04r border-grey-top border-grey-bottom">
+              <div class="ml05r mr05r pt04r">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">QBF Asset Management представляет Инвестиционный фонд недвижимости КьюБиЭф Недвижимость</a></p>
+                <p class="color-gray mt15 weight-300">Недвижимость с инвестиционной компанией QB Finance - Фонд недвижимости КьюБиЭф Недвижимость.</p>
+                <p class="color-teal mt15 weight-300">real-estate-gribovsky-les.html</p>
+              </div>
+              <div class="ml05r mr05r pt04r mt30">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">QBF Asset Management представляет Инвестиционный фонд недвижимости КьюБиЭф Недвижимость</a></p>
+                <p class="color-gray mt15 weight-300">Недвижимость с инвестиционной компанией QB Finance - Фонд недвижимости КьюБиЭф Недвижимость.</p>
+                <p class="color-teal mt15 weight-300">real-estate-gribovsky-les.html</p>
+              </div>
+              <div class="ml05r mr05r pt04r mt30">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">История компании - QB Finance</a></p>
+                <p class="color-gray mt15 weight-300">QB Finance - история компании. Работа на фондовых рынках США и Европы, консалтинговое управление активами.</p>
+                <p class="color-teal mt15 weight-300">history.html</p>
+              </div>
+              <div class="ml05r mr05r pt04r mt30">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">QB Finance - события компании - 15</a></p>
+                <p class="color-gray mt15 weight-300">QB Finance - обзор событий инвестиционной компании. Мероприятия компании.</p>
+                <p class="color-teal mt15 weight-300">qbfinance-sobyti.html</p>
+              </div>
+              <div class="ml05r mr05r pt04r mt30">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">История компании - QB Finance</a></p>
+                <p class="color-gray mt15 weight-300">QB Finance - история компании. Работа на фондовых рынках США и Европы, консалтинговое управление активами.</p>
+                <p class="color-teal mt15 weight-300">history.html</p>
+              </div>
+              <div class="ml05r mr05r pt04r mt30">
+                <p class="bold mt15 weight-300"><a href="#" class="gray-hover">Реквизиты компании - QB Finance</a></p>
+                <p class="color-gray mt15 weight-300">QB Finance - ваше лучшее финансовое будущее. Реквизиты компании.</p>
+                <p class="color-teal mt15 weight-300">banking_information.html</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+      <div class="text-center mt05r">
+        <p class="weight-300 uppercase size-12">ВСЕГО НАЙДЕНО: 50</p>
+        <p class="mt15">
+          <ul class="size-18">
+            <li class="inline weight-300"><a class="color-teal" href="#">1</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">2</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">3</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">4</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">5</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">6</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+            <li class="inline weight-300"><a href="#">7</a></li>
+            <li class="color-whitesmoke inline"><span>•</span></li>
+          </ul>
+        </p>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  <!-- FOOTER BLOCK -->
+    <div class="clearfix mt50"></div>
+    <div class="w100r">
+          <div class="text-center ">
+            <img id="lighthouse" src="img/icons/icon-lighthouse.svg" class="icon icon-main mt02r"><br>
+            <a href="/company.php" class="size-12 uppercase">Компания</a>
+          </div>    
+    </div>    
+    <!-- /FOOTER BLOCK -->
+    <?php include 'html/footer2.html';?>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/main.js"></script>
+  </body>
+</html>
+<!--
+      
+        <div class="column-offset bg-white">
+          
+        </div>
+        <div class="column-offset bg-white pt05r">
+          <h3 class="size-12 color-teal pt05r pb05r"> ТО ЧУВСТВО, КОГДА СДЕЛАЛ ПРАВИЛЬНЫЙ ВЫБОР </h3>
+        </div>
+      -->
---
+++ success.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QBF ||| investment - официальный сайт</title>
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/grid.css" rel="stylesheet">
+    <link href="/css/typo.css" rel="stylesheet">
+    <link href="/css/icons.css" rel="stylesheet">
+    <link href="/css/bg.css" rel="stylesheet">
+    <link href="/css/gallery.css" rel="stylesheet">
+    <link href="/css/blocks.css" rel="stylesheet">
+    <link href="/css/forms.css" rel="stylesheet">
+    <link href="/css/theme.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,100' rel='stylesheet' type='text/css'>
+    <!-- HTML5 for IE8 -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'html/header-connect.html';?>
+    <!-- HEADBLOCK -->
+    <div id="cover"
+      class="img-success-1 cover w100r h480 mt-80"
+      style="background-position: center bottom;">
+    </div>
+    <div class="mt-35 h35">
+      <div class="bg-white h35 ml05r mr05r">
+
+      </div>
+    </div>
+    <div class="text-center">
+      <div class="column-offset line-h20 bg-white pt02r">
+        <h3 class="size-12 color-teal pt02r"> ТО ЧУВСТВО, КОГДА СДЕЛАЛ ПРАВИЛЬНЫЙ ВЫБОР </h3>
+      </div>
+      <div class="column-offset line-h20 mt50">
+        <h3 class="size-12"> ВАШЕ СООБЩЕНИЕ БЫЛО УСПЕШНО ОТПРАВЛЕНО! </h3>
+      </div>
+      <div class="column-offset line-h20 mt25">
+        <h3 class="size-12"> В БЛИЖАЙШЕЕ ВРЕМЯ НАШИ СПЕЦИАЛИСТЫ СВЯЖУТСЯ С ВАМИ. </h3>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <!-- /HEADBLOCK -->
+  <!-- FOOTER BLOCK -->
+    <div class="clearfix mt50"></div>
+    <div class="w100r">
+          <div class="text-center ">
+            <img id="lighthouse" src="img/icons/icon-lighthouse.svg" class="icon icon-main mt02r"><br>
+            <span class="size-12 uppercase">ВЕРНУТЬСЯ НА СТРАНИЦУ:</span><a href="/connect.php" class="size-12 uppercase color-teal gray-hover">Контакты</a>
+          </div>    
+    </div>    
+    <!-- /FOOTER BLOCK -->
+    <?php include 'html/footer2.html';?>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/main.js"></script>
+  </body>
+</html>
+<!--
+      
+        <div class="column-offset bg-white">
+          
+        </div>
+        <div class="column-offset bg-white pt05r">
+          <h3 class="size-12 color-teal pt05r pb05r"> ТО ЧУВСТВО, КОГДА СДЕЛАЛ ПРАВИЛЬНЫЙ ВЫБОР </h3>
+        </div>
+      -->
